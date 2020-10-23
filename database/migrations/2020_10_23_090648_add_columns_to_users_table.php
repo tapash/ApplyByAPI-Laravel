1d93e804072d707574f382e7b6755f18908f38a5
Tapas Datta <tapash.me@gmail.com>
2020-10-23 15:30:08 +0600

    migration: added company columns into users tabel

---
+++ database/migrations/2020_10_23_090648_add_columns_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('company_name')->after('password')->default('');
+            $table->string('company_logo')->nullable()->after('company_name')->default('');
+            $table->string('company_website')->nullable()->after('company_logo')->default('');
+            $table->text('company_description')->nullable()->after('company_website')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'company_name',
+                'company_logo',
+                'company_website',
+                'company_description'
+            ]);
+        });
+    }
+}
